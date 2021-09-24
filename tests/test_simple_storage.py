ae50975cf0c70233b232ed6f1edd760b9477b3d8
yhy940806 <yhy940806@163.com>
2021-09-23 20:07:52 -0400

    – deploy and test script

---
+++ tests/test_simple_storage.py
@@ -0,0 +1,18 @@
+from brownie import SimpleStorage, accounts
+
+
+def test_deploy():
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.retrieve()
+    expected = 0
+
+    assert starting_value == expected
+
+
+def test_updating_storage():
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    expected = 15
+    simple_storage.store(expected, {"from": account})
+    assert 5 == simple_storage.retrieve()
